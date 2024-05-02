d884c505f6f162cdc81c7a9cadfed3892de9b98e
[vanshgusain] <vanshgusain18@gmail.com>
2024-05-02 19:57:51 +0530

    bug fixes: fixed the switch in router

--- src/App.js
+++ src/App.js
@@ -9,25 +9,28 @@ export default class App extends Component {
         <Router>
           <Navbar />
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <News country="in" category="general" pageSize="12" />
             </Route>
-            <Route path="/business">
+            <Route exact path="/general">
+              <News country="in" category="general" pageSize="12" />
+            </Route>
+            <Route exact path="/business">
               <News country="in" category="business" pageSize="12" />
             </Route>
-            <Route path="/entertainment">
+            <Route exact path="/entertainment">
               <News country="in" category="entertainment" pageSize="12" />
             </Route>
-            <Route path="/health">
+            <Route exact path="/health">
               <News country="in" category="health" pageSize="12" />
             </Route>
-            <Route path="/science">
+            <Route exact path="/science">
               <News country="in" category="science" pageSize="12" />
             </Route>
-            <Route path="/sports">
+            <Route exact path="/sports">
               <News country="in" category="sports" pageSize="12" />
             </Route>
-            <Route path="/technology">
+            <Route exact path="/technology">
               <News country="in" category="technology" pageSize="12" />
             </Route>
           </Switch>
