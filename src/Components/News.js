8dd6bd0e33f25a0b3b0f4734d4ad2ab27adfbbe1
[vanshgusain] <vanshgusain18@gmail.com>
2024-05-14 20:45:25 +0530

    updates: added infinite scoll

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
@@ -22,69 +23,64 @@ export class News extends Component {
       articles: [],
       loading: false,
       page: 1,
+      totalResults: 0,
     };
   }
 
-  async componentDidMount() {
-    let URL = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=b413b0db42c640379dbd0140109c87ef&page=1&category=${this.props.category}&pageSize=${this.props.pageSize}`;
+  async updateNews() {
+    const URL = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=b413b0db42c640379dbd0140109c87ef&page=${this.state.page}&category=${this.props.category}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(URL);
     let oData = await data.json();
     this.setState({
       articles: oData.articles,
       totalResults: oData.totalResults,
+      loading: false,
     });
   }
+  async componentDidMount() {
+    this.updateNews();
+  }
 
-  handleprev = async () => {
-    this.setState((prevState) => {
-      if (prevState.page > 1) {
-        let URL = `https://newsapi.org/v2/top-headlines?country=${
-          this.props.country
-        }&apiKey=b413b0db42c640379dbd0140109c87ef&category=${
-          this.props.category
-        }&page=${prevState.page - 1}&pageSize=${this.props.pageSize}`;
+  // handleprev = () => {
+  //   this.setState(
+  //     (prevState) => ({
+  //       page: prevState.page - 1,
+  //     }),
+  //     () => {
+  //       this.updateNews();
+  //     }
+  //   );
+  // };
+
+  // handlenex = () => {
+  //   this.setState(
+  //     (prevState) => ({
+  //       page: prevState.page + 1,
+  //     }),
+  //     () => {
+  //       this.updateNews();
+  //     }
+  //   );
+  // };
+
+  fetchMoreData = async () => {
+    this.setState(
+      (prevState) => ({
+        page: prevState.page + 1,
+      }),
+      async () => {
+        const URL = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=b413b0db42c640379dbd0140109c87ef&page=${this.state.page}&category=${this.props.category}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
-        fetch(URL)
-          .then((data) => data.json())
-          .then((oData) => {
-            this.setState({
-              page: prevState.page - 1,
-              articles: oData.articles,
-              loading: false,
-              totalResults: oData.totalResults,
-            });
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-            this.setState({ loading: false });
-          });
-      }
-    });
-  };
-
-  handlenex = async () => {
-    this.setState((prevState) => {
-      let URL = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&apiKey=b413b0db42c640379dbd0140109c87ef&category=${
-        this.props.category
-      }&page=${prevState.page + 1}&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      fetch(URL)
-        .then((data) => data.json())
-        .then((oData) => {
-          this.setState((prevState) => ({
-            page: prevState.page + 1,
-            articles: oData.articles,
-            totalResults: oData.totalResults,
-            loading: false,
-          }));
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-          this.setState({ loading: false });
+        let data = await fetch(URL);
+        let oData = await data.json();
+        this.setState({
+          articles: this.state.articles.concat(oData.articles),
+          totalResults: oData.totalResults,
+          loading: false,
         });
-    });
+      }
+    );
   };
 
   render() {
@@ -95,38 +91,47 @@ export class News extends Component {
             this.props.category.charAt(0).toUpperCase() +
             this.props.category.slice(1)
           } Headlines`}</h2>
-          {this.state.loading && this.state.loading && <Spinner />}
+          {/* {this.state.loading && <Spinner />} */}
         </div>
-        <div className="row">
-          {this.state.articles.map((ele) => {
-            return (
-              <div className="col-md-3" key={ele.url}>
-                {!this.state.loading && (
-                  <Newsitem
-                    tittle={ele.title ? ele.title.slice(0, 45) : ""}
-                    description={
-                      ele.description ? ele.description.slice(0, 88) : ""
-                    }
-                    imgUrl={
-                      ele.urlToImage
-                        ? ele.urlToImage
-                        : "https://img.freepik.com/premium-vector/no-result-found-empty-results-popup-design_586724-96.jpg?w=1060"
-                    }
-                    url={ele.url}
-                    source={ele.source.name}
-                    author={
-                      ele.author !== null && ele.author !== undefined
-                        ? ele.author
-                        : "unknown"
-                    }
-                    time={new Date(ele.publishedAt).toGMTString()}
-                  />
-                )}
-              </div>
-            );
-          })}
-        </div>
-        <div className="container d-flex justify-content-between">
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="row">
+            {this.state.articles.map((ele) => {
+              return (
+                <div className="col-md-3" key={ele.url}>
+                  {
+                    <Newsitem
+                      tittle={ele.title ? ele.title.slice(0, 45) : ""}
+                      description={
+                        ele.description ? ele.description.slice(0, 88) : ""
+                      }
+                      imgUrl={
+                        ele.urlToImage
+                          ? ele.urlToImage
+                          : "https://img.freepik.com/premium-vector/no-result-found-empty-results-popup-design_586724-96.jpg?w=1060"
+                      }
+                      url={ele.url}
+                      source={ele.source.name}
+                      author={
+                        ele.author !== null && ele.author !== undefined
+                          ? ele.author
+                          : "unknown"
+                      }
+                      time={new Date(ele.publishedAt).toGMTString()}
+                    />
+                  }
+                </div>
+              );
+            })}
+          </div>
+        </InfiniteScroll>
+
+        {/* <div className="container d-flex justify-content-between">
           <button
             disabled={this.state.page <= 1}
             type="button"
@@ -146,7 +151,7 @@ export class News extends Component {
           >
             next &rarr;
           </button>
-        </div>
+        </div> */}
       </div>
     );
   }
