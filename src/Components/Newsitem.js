6a61b98e01018a328a7d07f4c005130af912a1cc
[vanshgusain] <vanshgusain18@gmail.com>
2024-05-19 08:40:38 +0530

    updates: turned newsitem.js into function based component

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,39 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Newsitem extends Component {
-  render() {
-    let { tittle, description, imgUrl, url, time, author, source } = this.props;
-    return (
-      <div className="my-2">
-        <div className="card" style={{}}>
-          <img
-            src={imgUrl}
-            style={{ height: "9rem", backgroundSize: "cover" }}
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{tittle}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                By {author} on {time}
-              </small>
-            </p>
-            <a href={url} className="btn btn-primary btn-sm">
-              Read More
-            </a>
-            <span
-              className="position-absolute top-0  translate-middle badge rounded-pill bg-danger"
-              style={{ left: "50%" }}
-            >
-              {source}
-            </span>
-          </div>
+const Newsitem = (props) => {
+  let { tittle, description, imgUrl, url, time, author, source } = props;
+  return (
+    <div className="my-2">
+      <div className="card" style={{}}>
+        <img
+          src={imgUrl}
+          style={{ height: "9rem", backgroundSize: "cover" }}
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{tittle}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {author} on {time}
+            </small>
+          </p>
+          <a href={url} className="btn btn-primary btn-sm">
+            Read More
+          </a>
+          <span
+            className="position-absolute top-0  translate-middle badge rounded-pill bg-danger"
+            style={{ left: "50%" }}
+          >
+            {source}
+          </span>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Newsitem;
