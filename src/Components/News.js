bc92219987f02ed7a6b07847294920ccaa4edb65
[vanshgusain] <vanshgusain18@gmail.com>
2024-05-20 12:27:57 +0530

    updates: turned news.js into function based component

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,120 +1,103 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Newsitem from "./Newsitem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: "12",
-    category: "general",
-  };
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.string,
-    category: PropTypes.string,
-  };
+  useEffect(() => {
+    updateNews();
+  }, []);
 
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-    };
-  }
-
-  async updateNews() {
-    this.props.setProgress(10);
-    const URL = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=${this.props.apiKey}&page=${this.state.page}&category=${this.props.category}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  const updateNews = async () => {
+    props.setProgress(10);
+    const URL = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=${props.apiKey}&page=${page}&category=${props.category}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(URL);
-    this.props.setProgress(40);
+    props.setProgress(40);
     let oData = await data.json();
-    this.props.setProgress(70);
-    this.setState({
-      articles: oData.articles,
-      totalResults: oData.totalResults,
-      loading: false,
-    });
-    this.props.setProgress(100);
-  }
-  async componentDidMount() {
-    this.updateNews();
-  }
-
-  fetchMoreData = async () => {
-    this.setState(
-      (prevState) => ({
-        page: prevState.page + 1,
-      }),
-      async () => {
-        const URL = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=${this.props.apiKey}&page=${this.state.page}&category=${this.props.category}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(URL);
-        let oData = await data.json();
-        this.setState({
-          articles: this.state.articles.concat(oData.articles),
-          totalResults: oData.totalResults,
-        });
-      }
-    );
+    props.setProgress(70);
+    setArticles(oData.articles);
+    setTotalResults(oData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
   };
 
-  render() {
-    return (
-      <div className="container mt-2">
-        <div className="text-center mt-2">
-          <h2>{`InfoPulse: Top ${
-            this.props.category.charAt(0).toUpperCase() +
-            this.props.category.slice(1)
-          } Headlines`}</h2>
-          {this.state.loading && <Spinner />}
-        </div>
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    const URL = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=${props.apiKey}&page=${page}&category=${props.category}&pageSize=${props.pageSize}`;
+    let data = await fetch(URL);
+    let oData = await data.json();
+    setArticles(articles.concat(oData.articles));
+    setTotalResults(oData.totalResults);
+  };
 
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container">
-            <div className="row">
-              {this.state.articles.map((ele) => {
-                return (
-                  <div className="col-md-3" key={ele.url}>
-                    {
-                      <Newsitem
-                        tittle={ele.title ? ele.title.slice(0, 45) : ""}
-                        description={
-                          ele.description ? ele.description.slice(0, 88) : ""
-                        }
-                        imgUrl={
-                          ele.urlToImage
-                            ? ele.urlToImage
-                            : "https://img.freepik.com/premium-vector/no-result-found-empty-results-popup-design_586724-96.jpg?w=1060"
-                        }
-                        url={ele.url}
-                        source={ele.source.name}
-                        author={
-                          ele.author !== null && ele.author !== undefined
-                            ? ele.author
-                            : "unknown"
-                        }
-                        time={new Date(ele.publishedAt).toGMTString()}
-                      />
-                    }
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </InfiniteScroll>
+  return (
+    <div className="container mt-2">
+      <div className="text-center mt-2">
+        <h2>{`InfoPulse: Top ${
+          props.category.charAt(0).toUpperCase() + props.category.slice(1)
+        } Headlines`}</h2>
+        {loading && <Spinner />}
       </div>
-    );
-  }
-}
+
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((ele) => {
+              return (
+                <div className="col-md-3" key={ele.url}>
+                  {
+                    <Newsitem
+                      tittle={ele.title ? ele.title.slice(0, 45) : ""}
+                      description={
+                        ele.description ? ele.description.slice(0, 88) : ""
+                      }
+                      imgUrl={
+                        ele.urlToImage
+                          ? ele.urlToImage
+                          : "https://img.freepik.com/premium-vector/no-result-found-empty-results-popup-design_586724-96.jpg?w=1060"
+                      }
+                      url={ele.url}
+                      source={ele.source.name}
+                      author={
+                        ele.author !== null && ele.author !== undefined
+                          ? ele.author
+                          : "unknown"
+                      }
+                      time={new Date(ele.publishedAt).toGMTString()}
+                    />
+                  }
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 12,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 
 export default News;
