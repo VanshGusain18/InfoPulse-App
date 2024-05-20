177b4a0d5fe145eb9a7d21a4c08b9f64017c570a
[vanshgusain] <vanshgusain18@gmail.com>
2024-05-20 12:52:27 +0530

    updates: turned navbar to sticky navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             InfoPulse
